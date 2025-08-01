733cf06faa8ac3885f7d8efe7390678ee501f31c
Shikha <shikha479saini@gmail.com>
2025-08-01 23:21:09 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,70 @@
+//IIFE(IMMEDIATELY INVOKED FUNCTION EXPRESSION)
+// let a = ()=>{
+//     return new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//     resolve(456)
+//   },4000)
+// })
+// }
+// (async () => {
+//     let b = await a()
+//     console.log(b)
+//     let c = await a()
+//     console.log(c)
+//     let d = await a()
+//     console.log(d)
+// })
+// ()
+
+
+//destructuring
+
+// let arr=[3,5,47,8,9]
+// // let [c,b]=arr
+// // let [c,b,...rest]=arr
+// let [c, ,...rest]=arr
+// console.log(c,rest)
+
+//spread 
+let arr1 =[3,5,7]
+let obj = {...arr1}
+console.log(obj)
+
+function sum(v1,v2,v3){
+    return v1+v2+v3
+}
+console.log(sum(...arr1))
+
+let data={
+    name:"shikha",
+    age:"20",
+    id:"567"
+};
+// console.log({...data,name:"deepa"})
+console.log({name:"deepa",...data}) //this will print data object without changing
+
+//scope
+//block scope
+{
+    let a=8;
+    console.log(a)
+}
+// console.log(a)  //this shows error due to bock scope
+
+//function scope
+function ax(){
+    let a =67;
+    console.log(a)
+}
+ax()
+
+//HOSTING
+
+//following two lines will run successfully due to javascript hoisting
+console.log(a)
+greet()
+function greet(){
+    console.log("good morning")
+}
+var a = 9; // declaration hoisted to the top but initialization is not 
+console.log(a)
